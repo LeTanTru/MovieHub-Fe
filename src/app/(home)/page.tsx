32e2c5f20b55914860327f3f6adcfff5528a4ff6
TRU\Tru Le <letantru@gmail.com>
2025-08-20 23:08:02 +0700

    fix: test source tree

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,5 @@
 import Slider from '@/app/(home)/_components/slider/slider';
 
 export default async function HomePage() {
-  return (
-    <>
-      <Slider />
-    </>
-  );
+  return <>{/* <Slider /> */}</>;
 }
