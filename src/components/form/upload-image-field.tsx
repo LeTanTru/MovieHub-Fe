81ab01518f07c34b4742b422165a5ed2a26134a3
LeTanTru <letantru@gmail.com>
2026-01-27 19:55:54 +0700

    refactor: upload image field

--- src/components/form/upload-image-field.tsx
+++ src/components/form/upload-image-field.tsx
@@ -237,7 +237,8 @@ export default function UploadImageField<T extends FieldValues>({
               'border-input hover:bg-accent/50 focus-visible:border-ring relative flex cursor-pointer items-center justify-center overflow-hidden border border-dashed p-0 transition-colors outline-none focus-visible:ring-[3px]',
               className,
               {
-                'border border-solid border-red-500': !!error
+                'border border-solid border-red-500': !!error,
+                'border-none': !!value
               }
             )}
             onClick={openFileDialog}
@@ -254,7 +255,7 @@ export default function UploadImageField<T extends FieldValues>({
                 disablePreview
                 showHoverIcon={false}
                 src={value}
-                className='size-full rounded-none object-cover'
+                className='size-full rounded-none border-none object-cover'
                 aspect={aspect}
                 width={size * aspect}
                 height={size}
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useAppLoading } from './use-app-loading';
 export { default as useClickOutside } from './use-click-out-side';
+export { default as useDisclosure } from './use-disclosure';
 export { default as useFileUploadManager } from './use-file-upload-manager';
 export { default as useIsMounted } from './use-is-mounted';
 export { default as useMobile } from './use-mobile';
