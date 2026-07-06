54a84b4e6615d83664ffd60448c3b88973b4ab1a
LeTanTru <letantru@gmail.com>
2026-07-06 08:32:16 +0700

    feat: implement watch player video area component and configure local IDE permissions

--- src/components/app/watch/watch-player-video-area.tsx
+++ src/components/app/watch/watch-player-video-area.tsx
@@ -18,7 +18,7 @@ import { usePlayerSettings } from '@/app/watch/[slug]/_hooks';
 import { useState } from 'react';
 import { useVideoLibrarySubtitleListQuery } from '@/queries';
 import { useWatchPlayer } from '@/app/watch/[slug]/_context';
-import { VIDEO_SOURCE_TYPE_INTERNAL } from '@/constants';
+import { languages, VIDEO_SOURCE_TYPE_INTERNAL } from '@/constants';
 import { VideoLibrarySubtitleResType } from '@/types';
 import { VideoPlayer } from '@/components/video-player';
 import { WatchAskContinueModal } from './watch-ask-continue-modal';
@@ -84,7 +84,9 @@ export function WatchPlayerVideoArea() {
   const textTracks: TrackProps[] = videoLibrarySubtitles.map(
     (subtitle: VideoLibrarySubtitleResType) => ({
       src: renderVttUrl(video.hostname, subtitle.fileUrl, video.sourceType),
-      label: subtitle.label,
+      label:
+        languages.find((lang) => lang.value === subtitle.language)?.label ||
+        subtitle.label,
       language: subtitle.language,
       kind: 'subtitles',
       type: 'vtt',
